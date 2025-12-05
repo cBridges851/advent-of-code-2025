576d93998e3b3255a69172f6f7f9352e7b21362e
Christa Bridges <christa@lenovo>
2025-12-05 14:53:59 +0000

    Day 1 Part 2 Complete

--- AdventOfCode2025/Day1/RotationsCalculator.cs
+++ AdventOfCode2025/Day1/RotationsCalculator.cs
@@ -4,6 +4,7 @@
             // Set initial position as 50
             var position = 50;
             var howManyTimesZeroWasHit = 0;
+            Console.WriteLine(position);
 
             // For each rotation
             foreach (var rotation in rotations) { 
@@ -13,30 +14,45 @@
 
                 // If L, subtract the number from position
                 if (direction == 'L') {
-                    position = this.CalculateLeftPosition(position, distance);
+                    var calculationResult = this.CalculateLeftPosition(position, distance);
+                    position = calculationResult.NewPosition;
+                    howManyTimesZeroWasHit += calculationResult.HowManyTimesZeroWasHit;
+                    Console.WriteLine($"""
+                            {rotation}
+                            Position: {calculationResult.NewPosition}
+                            How Many Times Zero Was Hit: {calculationResult.HowManyTimesZeroWasHit}
+                            ---
+                        """);
                 }
 
                 // If R, add the number to position
                 if (direction == 'R') {
-                    position = this.CalculateRightPosition(position, distance);
+                    var calculationResult = this.CalculateRightPosition(position, distance);
+                    position = calculationResult.NewPosition;
+                    howManyTimesZeroWasHit += calculationResult.HowManyTimesZeroWasHit;
+                    Console.WriteLine($"""
+                            {rotation}
+                            Position: {calculationResult.NewPosition}
+                            How Many Times Zero Was Hit: {calculationResult.HowManyTimesZeroWasHit}
+                            ---
+                        """);
                 }
 
-                // If the position is 0, increment a counter
-                if (position == 0) {
-                    howManyTimesZeroWasHit++;
-                }
             }
 
             // Return the counter
             return howManyTimesZeroWasHit;
         }
 
-        private int CalculateLeftPosition(int position, string distanceAsString) {
+        private (int NewPosition, int HowManyTimesZeroWasHit) CalculateLeftPosition(int position, string distanceAsString) {
+            var originalPosition = position;
             var distance = int.Parse(distanceAsString);
+            var howManyTimesZeroWasHit = 0;
 
             // 100 is essentially a full rotation, so we can reduce the distance by 100 until it's less than 100
             while (distance > 99) {
                 distance -= 100;
+                howManyTimesZeroWasHit++;
             }
 
             position -= distance;
@@ -44,27 +60,42 @@
             // If the position is less than 0, add it to 99
             if (position < 0) {
                 position += 100;
+
+                if (originalPosition != 0) {
+                    howManyTimesZeroWasHit++;
+                }
             }
 
-            return position;
+            if (position == 0) {
+                howManyTimesZeroWasHit++;
+            }
+
+            return (position, howManyTimesZeroWasHit);
         }
 
-        private int CalculateRightPosition(int position, string distanceAsString) {
+        private (int NewPosition, int HowManyTimesZeroWasHit) CalculateRightPosition(int position, string distanceAsString) {
+            var originalPosition = position;
             var distance = int.Parse(distanceAsString);
+            var howManyTimesZeroWasHit = 0;
 
             // 100 is essentially a full rotation, so we can reduce the distance by 100 until it's less than 100
             while (distance > 99) {
                 distance -= 100;
+                howManyTimesZeroWasHit++;
             }
 
             position += distance;
 
-            // If the position is greater than 99, subtract 99
+            // If the position is greater than 99, subtract 100
             if (position > 99) {
                 position -= 100;
+
+                if (originalPosition != 0) {
+                    howManyTimesZeroWasHit++;
+                }
             }
 
-            return position;
+            return (position, howManyTimesZeroWasHit);
         }
     }
 }
--- AdventOfCode2025/Day1/WhenASeriesOfRotationsArePassedIn.cs
+++ AdventOfCode2025/Day1/WhenASeriesOfRotationsArePassedIn.cs
@@ -3,38 +3,38 @@
         private static readonly object[] testCases = {
             new object[] { 
                     new[] {
-                    "L68",
-                    "L30",
-                    "R48",
-                    "L5",
-                    "R60",
-                    "L55",
-                    "L1",
-                    "L99",
-                    "R14",
-                    "L82"
-                }, 3 
+                    "L68", // 50 - 68 -> 82, hit 0 here
+                    "L30", // 82 - 30 -> 52
+                    "R48", // 52 + 48 -> 0, hit 0 here
+                    "L5", // 0 - 5 -> 95
+                    "R60", // 95 + 60 -> 55, hit 0 here
+                    "L55", // 55 - 55 -> 0, hit 0 here
+                    "L1", // 0 - 1 -> 99
+                    "L99", // 99 - 99 -> 0, hit 0 here
+                    "R14", // 0 + 14 -> 14
+                    "L82" // 14 - 82 -> 32, hit 0 here
+                }, 6 
             },
             new object[] { 
                     new[] {
-                    "R93",
-                    "L43",
-                    "L34",
-                    "R89",
-                    "L55",
-                    "R30",
-                    "R32",
-                    "R296",
-                    "L58",
-                    "L100",
-                    "L201",
-                    "L99"
-                }, 5
+                    "R93", // 50 + 93 -> 43, hit 0 here
+                    "L43", // 43 -0 -> 0, hit 0 here
+                    "L34", // 0 - 34 -> 66
+                    "R89", // 66 + 89 -> 55, hit 0 here
+                    "L55", // 55 - 55 -> 0, hit 0 here
+                    "R30", // 0 + 30 -> 30
+                    "R32", // 30 + 32 -> 62
+                    "R296", // 62 + 96 -> 58, hit 0 here + x2 for the hundreds
+                    "L58", // 58 - 58 -> 0, hit 0 here
+                    "L100", // 0 - 100 -> 0, hit 0 here
+                    "L201", // 0 - 1 -> 99, hit 0 here + x2 for the hundreds
+                    "L99" // 99 - 99 -> 0, hit 0 here
+                }, 13
             },
         };
 
         [TestCaseSource(nameof(this.testCases))]
-        public void ThenItShouldCorrectlyOutputTheNumberOfTimesZeroWasHit(string[] input, int expectedNumberOfZeros) {
+        public void ThenItShouldCorrectlyOutputTheNumberOfTimesZeroWasHitInTotal(string[] input, int expectedNumberOfZeros) {
             var result = new RotationsCalculator().CalculateNumberOfTimesZeroWasHit(input);
             Assert.That(result, Is.EqualTo(expectedNumberOfZeros));
         }
