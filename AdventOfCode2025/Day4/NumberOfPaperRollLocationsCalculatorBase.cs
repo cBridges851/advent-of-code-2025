ec11ac02a6c15b45d0e435ee40a0e102d434f17d
Christa Bridges <christa@lenovo>
2026-01-01 16:24:16 +0000

    day4 Ensure correct top and bottom right are checked - copy pasta durp

--- AdventOfCode2025/Day4/NumberOfPaperRollLocationsCalculatorBase.cs
+++ AdventOfCode2025/Day4/NumberOfPaperRollLocationsCalculatorBase.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Day4 {
     internal class NumberOfPaperRollLocationsCalculatorBase {
         public readonly int MaximumAdjacentPaperRollsExclusive;
@@ -24,12 +20,12 @@ namespace Day4 {
         public char[] GetSurroundingCells(int currentRow, int currentCol, char[][] grid) {
             var topLeft = currentRow > 0 && currentCol > 0 ? grid[currentRow - 1][currentCol - 1] : ' ';
             var topMiddle = currentRow > 0 ? grid[currentRow - 1][currentCol] : ' ';
-            var topRight = currentRow > 0 && currentCol < grid[currentRow].Length - 1 ? grid[currentRow - 1][currentCol + 1] : ' ';
+            var topRight = currentRow > 0 && currentCol < grid[currentRow - 1].Length - 1 ? grid[currentRow - 1][currentCol + 1] : ' ';
             var middleLeft = currentCol > 0 ? grid[currentRow][currentCol - 1] : ' ';
             var middleRight = currentCol < grid[currentRow].Length - 1 ? grid[currentRow][currentCol + 1] : ' ';
             var bottomLeft = currentRow < grid.Length - 1 && currentCol > 0 ? grid[currentRow + 1][currentCol - 1] : ' ';
             var bottomMiddle = currentRow < grid.Length - 1 ? grid[currentRow + 1][currentCol] : ' ';
-            var bottomRight = currentRow < grid.Length - 1 && currentCol < grid[currentRow].Length - 1 ? grid[currentRow + 1][currentCol + 1] : ' ';
+            var bottomRight = currentRow < grid.Length - 1 && currentCol < grid[currentRow + 1].Length - 1 ? grid[currentRow + 1][currentCol + 1] : ' ';
 
             return new[] {
                 topLeft, topMiddle, topRight,
